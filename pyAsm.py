cce95ab9de34f55989d978fabc858efb52ab52d7
HishnikR <132210560+HishnikR@users.noreply.github.com>
2025-02-05 03:31:28 +0300

    Add files via upload

--- pyAsm.py
+++ pyAsm.py
@@ -1,12 +1,21 @@
 CODESIZE = 16384
+DATASIZE = 4096
 
 Labels = dict()
 Forwards = dict()
 
 codeptr = 0
-
 code = [0]*CODESIZE
 
+cycle = 0
+pc = 0
+retreg = 0
+zflag = 0
+reg = [0]*4
+data = [0]*DATASIZE
+datareg = 0
+
+led = 0
 
 def addcmd(x):
     global codeptr
@@ -53,8 +62,6 @@ def resolve(label):
     for lab in Forwards:
         if Forwards[lab] == label:
             code[lab] |= codeptr
-
-
 
 
 def asm(line):
@@ -122,6 +129,97 @@ def asm(line):
             print('Error: ', token[0])
 
 
+
+def make_cmd():
+    global cycle
+    global code
+    global pc
+    global reg
+    global data
+    global datareg
+    global retreg
+    global zflag
+    global led
+
+    trace = True
+
+    def inport(addr):
+        temp = 0
+        if addr == 20000:
+            temp = 3
+        return temp
+
+    def outport(addr, data):
+        if addr == 10000:
+            led = data
+            if trace:
+                print('LED=', led, 'at cycle ', cycle)
+
+    cycle += 1
+    cmd = code[pc]
+    pc += 1
+    optype = (cmd >> 19) & 31
+    dest = (cmd >> 16) & 3
+    src = (cmd >> 14) & 3
+    lit = cmd & 65535
+
+  #  print(pc, optype, dest, src, lit, reg)
+
+    match optype:
+        case 0:
+            pass
+        case 1:
+            pc = retreg
+        case 2:
+            pc = lit
+        case 3:
+            retreg = pc
+            pc = lit
+        case 4: # jmpnz
+            if zflag == 0:
+                pc = lit
+        case 5: # jmpz
+            if zflag == 1:
+                pc = lit
+        case 6:
+            data[reg[dest]] = reg[src]
+        case 7:
+            datareg = data[reg[dest]]
+        case 8:
+            reg[dest] = datareg
+        case 9:
+            reg[dest] = reg[dest] + reg[src]
+        case 10:
+            reg[dest] = reg[dest] - reg[src]
+        case 11:
+            if reg[dest] == reg[src]:
+                zflag = 1
+            else:
+                zflag = 0
+        case 12:
+            reg[dest] = inport(reg[src])
+        case 13:
+            outport(reg[dest], reg[src])
+        case 14:
+            reg[dest] = reg[dest] * reg[src]
+        case 15:
+            reg[dest] = reg[dest] << 1
+        case 16:
+            reg[dest] = reg[dest] >> 1
+        case 17:
+            reg[dest] += 1
+        case 18:
+            reg[dest] -= 1
+        case 19:
+            reg[dest] = reg[src]
+        case 20:
+            reg[dest] = (reg[dest] & (65535 << 16)) | lit
+        case 21:
+            reg[dest] = (reg[dest] & 65535) | (lit << 16)
+        case _:
+            print('Error in operation type at ', pc)
+
+
 def asm_lines(text):
     for line in text.split('\n'):
         line = line.strip()
@@ -131,37 +229,37 @@ def asm_lines(text):
 
 print('Assembling')
 asm_lines('''movl r0 0
-movl r1 1000000
-store r1 r0
+movl r1 5
+store r0 r1
 movl r0 1
-movl r1 2000000
-store r1 r0
+movl r1 10
+store r0 r1
+movl r3 1
 jmp main
 label delay
 dec r0
+movl r1 0
 cmp r0 r1
 jmpnz delay
 ret
 label main
-movl r1 1
 label loop
-movl r3 10000
-out r2 r3
-movl r3 20000
-in r0 r3
+movl r1 10000
+out r1 r3
+movl r1 20000
+in r0 r1
 movl r2 0
 load r1 r2
 mult r0 r1
 movl r2 1
 load r1 r2
 add r0 r1
-movl r1 0
 call delay
-shl r2
+shl r3
 movl r1 16
-cmp r2 r1
+cmp r1 r3
 jmpnz loop
-movl r2 1
+movl r3 1
 jmp loop''')
 
 
@@ -172,3 +270,5 @@ for i in range (0, codeptr):
 
 print(Labels)
 
+for i in range(0, 1000):
+    make_cmd()
